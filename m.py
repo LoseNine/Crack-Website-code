711fba5524afa60666da6f16d5e6006faf0f396d
losenine <losenine@163.com>
2020-05-20 15:41:38 +0800

    Initial commit

---
+++ m.py
@@ -0,0 +1,23 @@
+import ctypes
+class OCR:
+    def __init__(self):
+        self.dll=None
+        self.setDll()
+
+    def setDll(self):
+        #加载DLL
+        self.dll=ctypes.CDLL("./OCR.dll")
+
+    def getOCR(self,mcg,img):
+        #输入dat地址和图片地址
+        imgPath = img.encode('utf8')
+        img = ctypes.create_string_buffer(imgPath)
+
+        mcgPath = mcg.encode('utf8')
+        mcg = ctypes.create_string_buffer(mcgPath)
+        result=ctypes.string_at(self.dll.getOCR(mcg, img))
+        return result.decode('gb2312')
+
+if __name__ == '__main__':
+    c=OCR()
+    print(c.getOCR("D:/codes/DLL/mcg.dat","D:/codes/DLL/2bc2w.png"))
