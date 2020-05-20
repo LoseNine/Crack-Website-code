1a1dd787a9c44ddc22afe1122856a861894b030b
losenine <losenine@163.com>
2020-05-20 15:57:27 +0800

    test

--- test.py
+++ test.py
@@ -3,11 +3,9 @@ class OCR:
     def __init__(self):
         self.dll=None
         self.setDll()
-
     def setDll(self):
         #加载DLL
         self.dll=ctypes.CDLL("./OCR.dll")
-
     def getOCR(self,mcg,img):
         #输入dat地址和图片地址
         imgPath = img.encode('utf8')
