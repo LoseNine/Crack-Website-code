cf725486d9f261a7c7b7453ebdcedf3582578ccb
losenine <losenine@163.com>
2020-05-26 10:50:05 +0800

    训练一号店

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from core import test
 
 if __name__ == '__main__':
     ocr=test.OCR()
-    print('result:', ocr.getOCR("D:/codes/DLL/dat/wy.dat", "D:/codes/DLL/wytest_2anq.bmp"))
+    print('result:', ocr.getOCR("D:/codes/DLL/dat/yhd.dat", "D:/codes/DLL/testimg/yhdtest_2ejm.bmp"))
